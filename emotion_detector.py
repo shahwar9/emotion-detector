acb3158999090cbc0b2ef91177c6da79a2aefe6b
Shahwar Saleem <saleemshahwar@gmail.com>
2022-11-24 14:38:46 -0500

    Add parameterization to emotion_detector.

--- emotion_detector.py
+++ emotion_detector.py
@@ -1,9 +1,19 @@
 import cv2
 from deepface import DeepFace
 import numpy as np  #this will be used later in the process
+import argparse 
+from pprint import pprint
 
-imgpath = "examples/boy_sad.jpeg"  #put the image where this file is located and put its name here
-image = cv2.imread(imgpath)
+def detect_emotion(imgpath):
+    print(imgpath)
+    image = cv2.imread(imgpath)
 
-analyze = DeepFace.analyze(image, actions=['emotion'])  
-print(analyze)
+    analyze = DeepFace.analyze(image, actions=['emotion'])  
+    pprint(analyze)
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--img_path', metavar='I', type=str, help='Image file with face to detect emotion.')
+    args = parser.parse_args()
+
+    detect_emotion(args.img_path)
